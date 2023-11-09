4491d29852a3eec204153ba5bf93dc4f1b503010
Thomas Lemay <2230852@champlaincollege.qc.ca>
2023-11-08 19:17:52 -0500

    SelectedOperator enum, Operation_Click, and Equal event handler (partially) implemented

--- WPF Calculator/MainWindow.xaml.cs
+++ WPF Calculator/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,17 +27,25 @@ namespace WPF_Calculator
             DecimalBtn.Click += DecimalBtn_Click;
             ChangeSignBtn.Click += ChangeSignBtn_Click;
             PercentageBtn.Click += PercentageBtn_Click;
-
+            AllClearBtn.Click += AllClear_Click;
+            EqualBtn.Click += EqualBtn_Click;
 
 
         }
 
-
         double lastNumber;
-        
+
         double result;
 
-      
+        SelectedOperator selectedOperator;
+
+        internal enum SelectedOperator
+        {
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division
+        }
 
         private void NumberBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -44,13 +53,18 @@ namespace WPF_Calculator
             DisplayLabel.Content += button.Content.ToString();
         }
 
-        private void DecimalBtn_Click( object sender, RoutedEventArgs e)
+        private void AllClear_Click(object sender, RoutedEventArgs e)
         {
-            var button = sender as Button;
-            DisplayLabel.Content += button.Content.ToString();  
+            DisplayLabel.Content = string.Empty;
         }
 
-        private void ChangeSignBtn_Click (object sender, RoutedEventArgs e)
+        private void DecimalBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            DisplayLabel.Content += button.Content.ToString();
+        }
+
+        private void ChangeSignBtn_Click(object sender, RoutedEventArgs e)
         {
             var btn = sender as Button;
             DisplayLabel.Content = Double.Parse(DisplayLabel.Content.ToString()) * -1;
@@ -63,10 +77,60 @@ namespace WPF_Calculator
 
         }
 
+        private void Operation_Click(Object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            string textButton = DisplayLabel.Content.ToString();
+
+            switch (textButton)
+            {
+                case "+":
+                    selectedOperator = SelectedOperator.Addition;
+                    break;
+
+                case "-":
+                    selectedOperator = SelectedOperator.Subtraction;
+                    break;
+
+                case "*":
+                    selectedOperator = SelectedOperator.Multiplication;
+                    break;
+
+                case "/":
+                    selectedOperator = SelectedOperator.Division;
+                    break;
+            }
+
+        }
+
+        private void EqualBtn_Click(Object sender, RoutedEventArgs e)
+        {
+            result = 0;
+            lastNumber = 0;
 
 
+            switch (selectedOperator)
+            {
+                case SelectedOperator.Addition:
+                    result = MathService.add(operand1, operand2);
+                    break;
+                case SelectedOperator.Subtraction:
+                    result = MathService.subtract(operand1, operand2);
+                    break;
+                case SelectedOperator.Multiplication:
+                    result = MathService.multiply(operand1, operand2);
+                    break;
+                case SelectedOperator.Division:
+                    result = MathService.divide(operand1, operand2);
+                    break;
+            }
+
+
+
+        }
 
 
 
     }
+
 }
---
+++ WPF Calculator/MathService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Calculator
+{
+    internal static class MathService
+    {
+        public static double add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public static double multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public static double divide(double num1, double num2)
+        {
+            return num1/num2;
+        }
+
+
+
+
+    }
+}
